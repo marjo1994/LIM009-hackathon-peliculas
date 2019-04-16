9ce49afa112071259a127cc12558417a229b06de
gabriela2393 <gabymoncayozerga23@gmail.com>
2019-04-16 11:52:20 -0500

    intento mostrar pelicula por palabra acción

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+//ALMACENAMOS LAS SECCIONES EN CONSTANTES //
+const welcomePage=document.getElementById("welcome-page");
+const startButton= document.getElementById("start-button");
+const secondPage=document.getElementById("second-page");
+const paragraph=document.getElementById("paragraph");
+const select=document.getElementById("select");
+const footer =document.getElementById("footer");
+
+startButton.style.display="block";
+welcomePage.style.display="block"
+select.style.display="none";
+paragraph.style.display="none";
+footer.style.display="block";
+
+startButton.addEventListener("click",()=>{
+  startButton.style.display="none";
+  welcomePage.style.display="none";
+  select.style.display="block";
+  paragraph.style.display="block";
+  footer.style.display="block";
+});
+
+
+/*
 const apiKey = '789019a0'; // almacenando clave para obtener la data de la web OMDb API
 
 const baseURL = 'http://www.omdbapi.com/?apikey='+apiKey;
@@ -14,4 +38,6 @@ const url = baseURL + '&s='+searchBy+'&p='+page; //agregando parametros a la url
   console.log(json);
 }
 
-searchMovies("monster",2)
+searchMovies("acción",2);
+
+searchMovies("ficción",15);*/
