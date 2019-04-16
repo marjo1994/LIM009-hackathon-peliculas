92e624320ca0f1f855179b57a6ed252098c5b8f7
marjo1994 <m_rhl@live.com>
2019-04-16 11:49:40 -1100

    Intentos por mostrar data por palabra accion

---
+++ main.js
@@ -0,0 +1,27 @@
+const movies = document.getElementById('movies');
+const apiKey = '789019a0';
+const baseURL = 'http://www.omdbapi.com/?apikey='+apiKey;
+
+const searchMovies = async(searchBy,page) => {
+const url = baseURL +'&s='+ searchBy +'&page='+ page;
+    const response = await fetch(url);
+    const json = await response.json();
+    const result = JSON.parse(JSON.stringify(json));
+    return result.Search;    
+    }
+
+    const printData = data => {
+        let string = '';
+        for(let movie = 0; movie < data.length; movie++) {
+        string += `<div>
+        <p>Title:${movie.Title}</p>
+        <img src="${movie.Poster}" alt="poster">
+        </div>`
+        }
+        return movies.innerHTML = string;
+}
+
+    searchMovies('accion',1)
+    .then(data => printData(data));
+
+
